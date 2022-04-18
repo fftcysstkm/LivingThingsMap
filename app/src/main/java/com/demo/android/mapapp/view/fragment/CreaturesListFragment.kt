86d2dff3b4c002f90df4303f0023dda19c64e2fc
fftcysstkm <t.satobm3012@gmail.com>
2022-04-18 21:32:52 +0900

    ViewModelを導入

--- app/src/main/java/com/demo/android/mapapp/view/fragment/CreaturesListFragment.kt
+++ app/src/main/java/com/demo/android/mapapp/view/fragment/CreaturesListFragment.kt
@@ -7,17 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.demo.android.mapapp.R
-import com.demo.android.mapapp.data.Creature
-import com.demo.android.mapapp.data.DataSource
 import com.demo.android.mapapp.databinding.FragmentCreaturesListBinding
 import com.demo.android.mapapp.view.adapter.CreatureAdapter
-import java.time.LocalDateTime
-import java.util.*
+import com.demo.android.mapapp.viewmodel.CreaturesViewModel
 
 
 /**
@@ -25,35 +23,40 @@ import java.util.*
  */
 class CreaturesListFragment : Fragment() {
 
+    // 生き物の情報を管理するViewModel
+    private val viewModel: CreaturesViewModel by activityViewModels()
+
+    // バインディングクラス
     private var _binding: FragmentCreaturesListBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var recyclerView: RecyclerView
 
-    private lateinit var creatureList: List<Creature>
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // テスト用の生き物リストをセット
-        this.creatureList = DataSource().createCreaturesList()
     }
 
+    /**
+     * フラグメント生成時、viewbindingを設定
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCreaturesListBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        return binding.root
     }
 
+    /**
+     * View生成後、リストにアダプターとリスナーを設定
+     */
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // リサイクラービュー設定
         recyclerView = binding.creatureList
         recyclerView.setHasFixedSize(true)
-
         val layoutManager = LinearLayoutManager(context)
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.addItemDecoration(
@@ -62,7 +65,8 @@ class CreaturesListFragment : Fragment() {
                 layoutManager.orientation
             )
         )
-        // アダプターにデータ、タップで画面遷移リスナーを設定
+
+        // 生き物リストにオブザーバ設定、リストタップで画面遷移するリスナーを設定
         val navHostFragment =
             requireActivity().supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
@@ -71,31 +75,21 @@ class CreaturesListFragment : Fragment() {
             navController.navigate(action)
         })
         recyclerView.adapter = adapter
-        adapter.submitList(this.creatureList)
+        viewModel.creatures.observe(viewLifecycleOwner) { list ->
+            adapter.submitList(list)
+        }
 
-        // fabでリスト追加するリスナーを登録
-        val random = Random()
-        val now = LocalDateTime.now()
+        // fabにリストを追加するリスナーを登録
         val fab = _binding?.addFab?.setOnClickListener {
-            val nextCreatures = this.creatureList.toMutableList()
-            val nextId = nextCreatures.size + 1
-            val position = random.nextInt(creatureList.size)
-            nextCreatures.add(position, Creature(id = nextId, type = "魚", "お魚 $nextId", now))
-            this.creatureList = nextCreatures
-            adapter.submitList(this.creatureList)
-
+            viewModel.addTestCreature()
         }
     }
 
     /**
-     * フラグメント終了時
+     * フラグメント終了時にバインディングクラス破棄
      */
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    companion object {
-
     }
 }
