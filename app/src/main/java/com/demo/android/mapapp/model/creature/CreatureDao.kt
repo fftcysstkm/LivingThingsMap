74be3884ee913fc271a16fbffec329002e5d12c8
fftcysstkm <t.satobm3012@gmail.com>
2022-06-05 19:11:35 +0900

    位置情報のstateの型を変更

--- app/src/main/java/com/demo/android/mapapp/model/creature/CreatureDao.kt
+++ app/src/main/java/com/demo/android/mapapp/model/creature/CreatureDao.kt
@@ -25,6 +25,8 @@ interface CreatureDao {
 
     /**
      * 生き物をIDで取得
+     * @param creatureId 生き物ID
+     * @return Creature 生き物クラス
      */
     @SuppressWarnings(RoomWarnings.CURSOR_MISMATCH)
     @Query("SELECT creatureId, categoryId, creatureName FROM Creature WHERE creatureId = :creatureId")
@@ -32,14 +34,28 @@ interface CreatureDao {
 
     /**
      * 生き物リストに表示する生き物を登録
+     * @param creature 生き物クラス
      */
     @Insert
     suspend fun addCreature(creature: Creature)
 
     /**
      * 生き物の詳細情報（位置情報）を登録
+     * @param creatureDetail 生き物詳細情報クラス
      */
     @Insert
     suspend fun addCreatureDetail(creatureDetail: CreatureDetail)
+
+    /**
+     * 生き物の詳細情報（位置情報）を取得
+     * @param creatureId 生き物ID
+     */
+    @Query(
+        "SELECT creatureDetailId, creatureId, creatureNum, detailMemo, recordedAt, longitude, latitude " +
+                "FROM CreatureDetail " +
+                "WHERE creatureId = :creatureId"
+    )
+    fun getCreatureDetails(creatureId: Long): Flow<List<CreatureDetail>>
+
 
 }
