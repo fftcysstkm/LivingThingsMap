6c6c468a729465a8a98904f0ea626a270e24e5b2
fftcysstkm <t.satobm3012@gmail.com>
2022-08-07 22:57:06 +0900

    TopAppBarにDropdownMenu追加

--- app/src/main/java/com/demo/android/mapapp/ui/component/TopBar.kt
+++ app/src/main/java/com/demo/android/mapapp/ui/component/TopBar.kt
@@ -1,8 +1,14 @@
 package com.demo.android.mapapp.ui.component
 
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.demo.android.mapapp.R
 
@@ -11,7 +17,35 @@ import com.demo.android.mapapp.R
  */
 @Composable
 fun TopBar() {
-    TopAppBar(title = {
-        Text(text = stringResource(id = R.string.app_name))
+
+    // ドロップダウンメニューを表示するかどうか（初期設定は表示しない。）
+    var isDisplayMenu by remember{ mutableStateOf(false)}
+
+    TopAppBar(title = {Text(text = stringResource(id = R.string.app_name))},
+    actions = {
+
+        Box{
+
+            // オプションメニュー（編集か削除のドロップダウンメニュー表示）
+            IconButton(onClick = { isDisplayMenu = !isDisplayMenu }) {
+                Icon(Icons.Default.MoreVert, "")
+            }
+
+            // ドロップダウンメニュー
+            DropdownMenu(expanded = isDisplayMenu, onDismissRequest = { isDisplayMenu = false }) {
+                // 編集（生きもの一覧リストにラジオボタンを表示、画面下部に「編集ボタン」配置）
+                DropdownMenuItem(onClick = { /*TODO*/ }) {
+                    Text(stringResource(id = R.string.drop_down_menu_edit))
+                }
+
+                // 削除（いきもの一覧リストにチェックボックスを表示、画面下部に「削除ボタン」配置）
+                DropdownMenuItem(onClick = { /*TODO*/ }) {
+                    Text(stringResource(id = R.string.drop_down_menu_delete))
+                }
+            }
+        }
+
     })
+
+
 }
